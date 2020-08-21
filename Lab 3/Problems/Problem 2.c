3e9875cacb4556247840effd9b53a470e6fc22b2
Mysum202 <70034697+Mysum202@users.noreply.github.com>
2020-08-22 02:20:33 +0600

    Add files via upload

---
+++ Lab 3/Problems/Problem 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    int a;
+    scanf("%d",&a);
+
+    if (a % 7 == 0)
+    {
+        printf("multiple by 7:   ");
+
+    }
+    else
+    {
+       printf("non multiple by 7:  ");
+    }
+
+    }
+
---
+++ Lab 3/Sample/Sample 2.c
@@ -0,0 +1,7 @@
+ #include<stdio.h>
+ int main()
+ {
+ if(0)
+printf("The statement is true");
+ return 0;
+ }
